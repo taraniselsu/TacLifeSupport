93d31b4473a830f0ac4530a70c27faf850624b7c
taraniselsu <taraniselsu@gmail.com>
2013-11-16 11:58:35 -0800

    Fixed the project file.
    Did not reference the KSP environment variable properly.

--- package.bat
+++ package.bat
@@ -1,3 +1,9 @@
+@if "%KSP%"=="" (
+	echo KSP has not been set!
+	pause
+	exit 1
+)
+
 set Path=%Path%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
 MSBuild /p:Configuration=Release /target:Package Source\TacLifeSupport.csproj
 pause
