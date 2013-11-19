383586722fa2b2f39019e0aac2268a5d5d41f5e9
taraniselsu <taraniselsu@gmail.com>
2013-11-18 23:33:04 -0800

    Changed the package.bat for consistency with my other mods.

--- package.bat
+++ package.bat
@@ -1,3 +1,5 @@
+set MOD_NAME=TacLifeSupport
+
 @if "%KSP%"=="" (
 	echo KSP has not been set!
 	pause
@@ -5,5 +7,5 @@
 )
 
 set Path=%Path%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
-MSBuild /p:Configuration=Release /target:Package Source\TacLifeSupport.csproj
+MSBuild /p:Configuration=Release /target:Package Source\%MOD_NAME%.csproj
 pause
